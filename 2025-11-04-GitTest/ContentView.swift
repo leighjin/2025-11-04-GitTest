0ee31022c8ad34dde811f8051d27b0dd3fd1a76d
leighjin <jinlei@sfsu.edu>
2025-11-05 10:18:33 -0800

    Initial Commit

---
+++ 2025-11-04-GitTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2025-11-04-GitTest
+//
+//  Created by Leigh Jin on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2025-11-04-GitTest/_025_11_04_GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _025_11_04_GitTestApp.swift
+//  2025-11-04-GitTest
+//
+//  Created by Leigh Jin on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct _025_11_04_GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
